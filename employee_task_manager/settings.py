879d86ffa17144c37453f87d3b22c9c6ee46f09d
thekidmellow <thekidmellow@gmail.com>
2025-08-27 16:31:16 +0200

    Configure Django messages framework with Bootstrap classes

--- employee_task_manager/settings.py
+++ employee_task_manager/settings.py
@@ -120,3 +120,13 @@ LOGIN_URL = 'accounts:login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+# Messages framework
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
