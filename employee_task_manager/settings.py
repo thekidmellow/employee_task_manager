acf5ebe92363e99dd89eb9f49b712a84afa546c5
thekidmellow <thekidmellow@gmail.com>
2025-08-27 16:23:14 +0200

    Configure middleware and root URLs

--- employee_task_manager/settings.py
+++ employee_task_manager/settings.py
@@ -29,3 +29,17 @@ INSTALLED_APPS = [
     'tasks',
     'core',
 ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # For static files on Heroku
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'employee_task_manager.urls'
+
