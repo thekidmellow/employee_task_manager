c7e4ce98b41ef1a55d1025246aebbae5dfb65124
thekidmellow <thekidmellow@gmail.com>
2025-08-27 16:27:13 +0200

    Add authentication password validators

--- employee_task_manager/settings.py
+++ employee_task_manager/settings.py
@@ -78,3 +78,19 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(config('DATABASE_URL'))
     }
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
