33935fd5e27f9506737584e4dcb9518db81071b3
thekidmellow <thekidmellow@gmail.com>
2025-08-27 14:52:03 +0200

    Init views.py with docstring and imports

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from .forms import UserRegistrationForm, UserProfileForm
+from .models import UserProfile
