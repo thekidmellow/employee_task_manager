287b9eb04f4b1ce5e1e92cbf4747d8e802527972
thekidmellow <thekidmellow@gmail.com>
2025-08-27 16:39:35 +0200

    Serve static & media files in development

--- employee_task_manager/urls.py
+++ employee_task_manager/urls.py
@@ -25,3 +25,8 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
