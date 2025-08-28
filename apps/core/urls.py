c5a59bead46c0239ddd3f4ee9db3dbeaa0ebfbe4
thekidmellow <thekidmellow@gmail.com>
2025-08-28 13:34:01 +0200

    Add home route for core app

---
+++ apps/core/urls.py
@@ -0,0 +1,11 @@
+"""
+URL patterns for core application views
+"""
+
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
