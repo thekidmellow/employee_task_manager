838e111bf324e275a0c9989aafb945648c9e31ae
thekidmellow <thekidmellow@gmail.com>
2025-08-28 13:35:11 +0200

    Add home route and manager dashboard route

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -9,3 +9,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('dashboard/manager/', views.manager_dashboard, name='manager_dashboard'),
+    path('dashboard/employee/', views.employee_dashboard, name='employee_dashboard'),
+]
