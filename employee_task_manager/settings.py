5d4abca3c2842498cb674fbe2bf1b22d722deaa3
thekidmellow <thekidmellow@gmail.com>
2025-09-04 16:01:16 +0200

    Heroku prep env SECRET_KEY, DEBUG via env, DB via DATABASE_URL, static w/ WhiteNoise"

--- employee_task_manager/settings.py
+++ employee_task_manager/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = "-Z3y1eO8ZDTUW-kVhFZsQzoLHDyuNk3MNEBJbN1nlzSZK5J3B5juh8V6psK_vRR97Fg"
+SECRET_KEY = config('SECRET_KEY', default='dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
