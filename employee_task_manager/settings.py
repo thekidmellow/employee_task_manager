7a6a29b601ca6653874d7de93bf670e53f7f50e7
thekidmellow <thekidmellow@gmail.com>
2025-09-25 20:34:19 +0200

    Set debug False to test heroku deployment

--- employee_task_manager/settings.py
+++ employee_task_manager/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY", default="dev-only")
 
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
